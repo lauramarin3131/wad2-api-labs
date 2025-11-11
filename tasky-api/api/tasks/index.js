6b9e74392a4166d9282a88df1dcd2483a6049082
lauramarin3131 <lauramarin3131@gmail.com>
2025-11-11 12:20:50 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -20,13 +20,16 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+     const timestamp = new Date().toISOString();
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: timestamp,
+        updated_at: timestamp
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -39,7 +42,7 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id,updated_at: new Date().toISOString()  };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
