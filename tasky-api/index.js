5c0c863340dd03ce9747aaf95ca5e07f6e334125
lauramarin3131 <lauramarin3131@gmail.com>
2025-11-25 11:22:44 +0000

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+//... other imports
+import usersRouter from './api/users';
+
 import './db';
 // other imports
 import cors from 'cors';
@@ -24,6 +27,7 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+app.use('/api/users', usersRouter);
 app.use(errHandler);
 
 app.listen(port, () => {
